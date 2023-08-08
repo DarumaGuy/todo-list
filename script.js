3452dfad4bb215c028654365759b5b68f46aa744
Renan Silva <darumarts@gmail.com>
2023-08-08 15:32:02 -0300

    todo-list

---
+++ script.js
@@ -0,0 +1,42 @@
+const list = document.querySelector('[data-list]')
+const input = document.querySelector('[data-input]')
+const addButton = document.querySelector('[data-button="add"]')
+
+function removeListItem (id) {
+    const itemToBeRemoved = document.querySelector(`[data-id="${id}"]`)
+    itemToBeRemoved.remove()
+}
+
+function addListItem () {
+    const idNumber = list.childElementCount + 1
+    const text = input.value
+    const item = document.createElement('li')
+    
+    item.setAttribute('data-id', idNumber)
+    item.innerHTML = `
+        <input type="checkbox" name="" id="item-${idNumber}" class="checkbox__input">
+    `
+
+    const label = document.createElement('label')
+    label.innerHTML = `
+        <div class="checkbox__icon-wrapper">
+            <img class="checkbox__icon" src="check-icon.png" alt="">
+        </div>
+        <input class="checkbox__text">
+    `
+
+    label.setAttribute('for', `item-${idNumber}`)
+    label.classList.add('checkbox__label')
+
+    const deleteButton = document.createElement('button')
+    deleteButton.classList.add('button--delete')
+    deleteButton.innerHTML = '<img class="checkbox__icon checkbox__icon--delete" src="delete-icon.png"/>'
+    deleteButton.addEventListener('click', () => removeListItem(idNumber))
+
+    label.appendChild(deleteButton)
+    item.appendChild(label)
+    list.prepend(item)
+}
+
+addButton.addEventListener('click', addListItem)
+
